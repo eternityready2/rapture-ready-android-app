a2429b4cd71c684d9fcdcf877f3fde897ea7da7e
appsoft124 <itsolutions.infoe@gmail.com>
2024-06-23 09:57:29 +0500

    icon changed

--- app/src/main/java/com/wRaptureReadyEndTimesNewsProphecyDoctrineofPreTribRapture/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/wRaptureReadyEndTimesNewsProphecyDoctrineofPreTribRapture/services/MyFirebaseMessagingService.java
@@ -9,6 +9,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import androidx.core.app.NotificationCompat;
 
@@ -35,9 +37,14 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
 
+        // Convert drawable resource to Bitmap
+        Bitmap largeIconBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.notification_logo);
+
+
         String channelId = "Default_Channel";
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
-                .setSmallIcon(R.drawable.product_logo) // Add your app's notification icon here
+                .setSmallIcon(R.drawable.notification_logo) // Add your app's notification icon here
+                .setLargeIcon(largeIconBitmap) // Add your app's notification icon here
                 .setContentTitle(remoteMessage.getNotification().getTitle())
                 .setContentText(remoteMessage.getNotification().getBody())
                 .setAutoCancel(true)
